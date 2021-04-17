518682b0516d08ab4cf380c03438e653d618810a
Chakshu kishnani <chakshu05kishnani@gmail.com>
2021-04-17 11:57:19 +0530

    Pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -7,11 +7,22 @@ const path = require("path");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
+	const currentPage = req.query.page || 1;
+	const perPage = 2;
+	let totalItems;
 	Post.find()
+		.countDocuments()
+		.then((count) => {
+			totalItems = count;
+			return Post.find()
+				.skip((currentPage - 1) * perPage)
+				.limit(perPage);
+		})
 		.then((posts) => {
 			res.status(200).json({
 				message: "Fetched PostsSuccessfully.",
 				posts: posts,
+				totalItems: totalItems,
 			});
 		})
 		.catch((err) => {
